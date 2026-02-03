242ef9bc0f5270c78df4f95edf90adc732c7166f
Ander Ayala <ayalaander231@gmail.com>
2026-02-03 16:53:24 -0500

    Add comments to GPA calculator code

--- Week-1/gpa calculator.cpp
+++ Week-1/gpa calculator.cpp
@@ -12,15 +12,15 @@ using namespace std;
 int main()
 
 {
-    int num1{};
-    int num2{};
-    int num3{};
-    int sum;
-    int quotient;
-        cout << "input 3 grades";
-        cin >> num1 >> num2 >> num3;
-    sum = num1 + num2 + num3;
-    quotient = sum / 3;
-    cout << "average gpa " << quotient << endl;
+    int num1{}; // first number
+    int num2{}; // second number
+    int num3{}; // third number 
+    int sum; // sum 
+    int quotient; // quotient 
+        cout << "input 3 grades"; // out for to let user know to input 
+        cin >> num1 >> num2 >> num3; // input from users for 3 numbers 
+    sum = num1 + num2 + num3; // adding of the 3 numbers 
+    quotient = sum / 3; // dividing the 3 numbers 
+    cout << "average gpa " << quotient << endl; // outputing the average of the numbers
     return 0;
 }

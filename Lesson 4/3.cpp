6133dc5d005891a5da35d34fb06404a56bee753d
Ander Ayala <ayalaander231@gmail.com>
2026-03-30 08:06:28 -0400

    Add files via upload

---
+++ Lesson 4/3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (unsigned int count = 1; count <= 10; ++count)
+    {
+        cout << count << " ";
+    }
+    return 0;
+}
+
+
+
+// // /*
+// // Reflection:
+// // 1. What I understood from this program:
+// //  (Explain the concept in your own words.)
+// This Program is just counting from 1 to 10 and printing the numbers out
+// // 2. Difficulties I faced:
+// //  (Syntax, logic errors, infinite loops, casting issues, etc.)
+// No issues with this program it was pretty straight forward
+// // 3. What I needed to search or review:
+// //  (Documentation, examples, lecture slides, etc.)
+// Maybe just the implimentation of the for loop but other than that it was pretty straight forward
+// // 4. AI Usage (if any):
+// //  - Did you use AI tools? If yes, explain how.
+// //  - Did you verify the output?
+// No Ai was used 
+// // 5. What I learned:
+// //  (Be specific about loops, counters, casting, etc.)
+// I learned how to use a for loop and how it works with the counter to count from 1 to 10
